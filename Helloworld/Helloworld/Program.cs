b324dc0ff9e393d93f4f50e2f6facc769ef87c31
Jackaren22 <Jackaren22@gmail.com>
2020-08-28 12:45:49 +0200

    Variabler och Datatyper i C#
    Uppgift 1 ( Om man inte räknar med Hello World uppgiften då såklart)

--- Helloworld/Helloworld/Program.cs
+++ Helloworld/Helloworld/Program.cs
@@ -7,6 +7,27 @@ namespace Helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Nikklas!");
+
+            Console.WriteLine("Please input your age.");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("Now input your name.");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("And lastly, are you currently alive? (Yes or No)");
+            string Alive = Console.ReadLine();
+
+            Console.WriteLine("You are " + Age + " years old | " + "Your name is " + Name + " | Currently Alive? " + Alive);
+
+            Console.WriteLine("BONUS: Input two different Numbers and i will tell you which one is the largest one. ");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+            Convert.ToInt32(Number1);
+
+            Console.WriteLine(Math.Max(Number1, Number2) + "Is the largest number");
+
+
+
         }
     }
 }
