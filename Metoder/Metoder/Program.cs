ea2a28fe450e83d10c6eb3222efd00d2294892ff
Jackaren22 <Jackaren22@gmail.com>
2020-09-11 15:24:45 +0200

    Metoder 2.a färdig
    2.b och 2.c är oklara men första är färdig

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 3, 5, 7, 9, 12 };
+            string[] words = { "hej", "svejs", "leif", "kommer", "majs" };
+
+            Console.WriteLine(sum(numbers));
+            Console.WriteLine(reversed(words));
+
+        }
+
+        static int sum(int[] numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        static string reversed(string[] words)
+        {
+            Array.Reverse(words);
+            return words[0];
+
+        }
+   
+    }
+}
