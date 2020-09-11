4d1ea1fdfb1c7378ddece953569430fba2f82df0
Jackaren22 <Jackaren22@gmail.com>
2020-09-11 14:31:40 +0200

    Listor Och Arrayer
    woopie woopie

---
+++ Listor och Arrayer/Listor och Arrayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Listor_och_Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[5];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Input a name 5 times:");
+                names[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(names);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(names[i]);
+                }
+
+
+            }
+        }
+    }
+
