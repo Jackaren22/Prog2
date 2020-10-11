cf706b58a9d0c782cde81b362dcce3fbf437b844
Jackaren22 <Jackaren22@gmail.com>
2020-10-11 22:14:26 +0200

    Nikklas Kod Kommenterad
    Tog koden Nikklas visade och kommenterade på hur den fungerar

---
+++ Lab 1 OOP Nikklas Kod/Lab 1 OOP Nikklas Kod/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1_OOP_Nikklas_Kod
+{
+    class Customer
+    {
+        public List<Product> _cart = new List<Product>(); // En kundvagn där produkter kommer att läggas in i 
+    }
+}
---
+++ Lab 1 OOP Nikklas Kod/Lab 1 OOP Nikklas Kod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_1_OOP_Nikklas_Kod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kund = new Customer(); // Variable för ny kund
+            while (true)
+            {
+                Console.WriteLine("Do you want to make a purchase?");
+                if (Console.ReadLine().ToLower() == "yes") 
+                {
+                    Console.WriteLine("What do you want to buy?");
+                    kund._cart.Add(new Product() { _productName = Console.ReadLine() }); // Här skapas en produkt som använderen själv skapar.
+                }
+                else
+                   break;
+
+                Console.WriteLine("Do you want to view your cart?");
+                if (Console.ReadLine().ToLower() == "yes")
+                    foreach (var item in kund._cart)  // För varje item(Produkt) i kundvagnen kommer den att skriva ut alla produkter i arrayen.
+                        Console.WriteLine(item._productName);
+                else
+                    Console.WriteLine("Why would you not check your cart... D:");
+                    Console.WriteLine();
+
+            }
+        }
+    }
+}
