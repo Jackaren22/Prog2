1fc45d85530996b1bca8e30ab327e5c66af0f8be
Jackaren22 <Jackaren22@gmail.com>
2020-11-16 12:50:04 +0100

    Knappar ja
    Nu funkar de flesta knapparna mycket fint. dags att få operationer att fungera

--- WPFMiniräknare/WPFMiniräknare/MainWindow.xaml.cs
+++ WPFMiniräknare/WPFMiniräknare/MainWindow.xaml.cs
@@ -24,5 +24,76 @@ namespace WPFMiniräknare
         {
             InitializeComponent();
         }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += PlusButton.Content;
+        }
+        private void MinusButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += MinusButton.Content;
+        }
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += MultiplyButton.Content;
+        }
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += DivideButton.Content;
+        }
+        private void DecimalButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += DecimalButton.Content;
+        }
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = "Yeah uh idk";
+        }
+
+        private void OneButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += OneButton.Content;
+        }
+        private void TwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += TwoButton.Content;
+        }
+        private void ThreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += ThreeButton.Content;
+        }
+        private void FourButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += FourButton.Content;
+        }
+        private void FiveButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += FiveButton.Content;
+        }
+        private void SixButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += SixButton.Content;
+        }
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += SevenButton.Content;
+        }
+        private void EightButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += EightButton.Content;
+        }
+        private void NineButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += NineButton.Content;
+        }
+        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += ZeroButton.Content;
+        }
+
     }
 }
